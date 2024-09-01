3e8e7f97963e3b6223f00abb4a9460f177fc6d92
cgorvalan <gorvalancandela@gmail.com>
2024-09-01 17:15:34 -0300

    1er-modificacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { EstadosCocherasComponent } from './pages/estados-cocheras/estados-cocheras.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login", 
+        component: LoginComponent
+    },
+    {
+        path: "estados-cocheras",
+        component: EstadosCocherasComponent
+    },
+    {
+        path:"",
+        redirectTo: "login",
+        pathMatch: "full"
+    }
+];
---
+++ src/app/pages/estados-cocheras/estados-cocheras.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-estados-cocheras',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './estados-cocheras.component.html',
+  styleUrl: './estados-cocheras.component.scss'
+})
+export class EstadosCocherasComponent {
+
+}
